b74eb11bba3ecea95787f6844ed2ca86ec53ae48
Johnathon Gard <johnathon.gard@colorado.edu>
2016-05-22 22:21:37 -0600

    Added a gui part, integraction WIP
    Working on getting a gui to actually work like a gui.

---
+++ dynamic_classes_python/gui.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(333, 256)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.but_read_reg_0 = QtGui.QPushButton(self.centralwidget)
+        self.but_read_reg_0.setObjectName(_fromUtf8("but_read_reg_0"))
+        self.gridLayout.addWidget(self.but_read_reg_0, 0, 0, 1, 1)
+        self.line_reg_0 = QtGui.QLineEdit(self.centralwidget)
+        self.line_reg_0.setObjectName(_fromUtf8("line_reg_0"))
+        self.gridLayout.addWidget(self.line_reg_0, 0, 1, 1, 1)
+        self.but_read_reg_1 = QtGui.QPushButton(self.centralwidget)
+        self.but_read_reg_1.setObjectName(_fromUtf8("but_read_reg_1"))
+        self.gridLayout.addWidget(self.but_read_reg_1, 1, 0, 1, 1)
+        self.line_reg_1 = QtGui.QLineEdit(self.centralwidget)
+        self.line_reg_1.setObjectName(_fromUtf8("line_reg_1"))
+        self.gridLayout.addWidget(self.line_reg_1, 1, 1, 1, 1)
+        self.but_read_reg_2 = QtGui.QPushButton(self.centralwidget)
+        self.but_read_reg_2.setObjectName(_fromUtf8("but_read_reg_2"))
+        self.gridLayout.addWidget(self.but_read_reg_2, 2, 0, 1, 1)
+        self.line_reg_2 = QtGui.QLineEdit(self.centralwidget)
+        self.line_reg_2.setObjectName(_fromUtf8("line_reg_2"))
+        self.gridLayout.addWidget(self.line_reg_2, 2, 1, 1, 1)
+        self.but_read_reg_3 = QtGui.QPushButton(self.centralwidget)
+        self.but_read_reg_3.setObjectName(_fromUtf8("but_read_reg_3"))
+        self.gridLayout.addWidget(self.but_read_reg_3, 3, 0, 1, 1)
+        self.line_reg_3 = QtGui.QLineEdit(self.centralwidget)
+        self.line_reg_3.setObjectName(_fromUtf8("line_reg_3"))
+        self.gridLayout.addWidget(self.line_reg_3, 3, 1, 1, 1)
+        self.but_read_reg_4 = QtGui.QPushButton(self.centralwidget)
+        self.but_read_reg_4.setObjectName(_fromUtf8("but_read_reg_4"))
+        self.gridLayout.addWidget(self.but_read_reg_4, 4, 0, 1, 1)
+        self.line_reg_4 = QtGui.QLineEdit(self.centralwidget)
+        self.line_reg_4.setObjectName(_fromUtf8("line_reg_4"))
+        self.gridLayout.addWidget(self.line_reg_4, 4, 1, 1, 1)
+        self.but_read_reg_5 = QtGui.QPushButton(self.centralwidget)
+        self.but_read_reg_5.setObjectName(_fromUtf8("but_read_reg_5"))
+        self.gridLayout.addWidget(self.but_read_reg_5, 5, 0, 1, 1)
+        self.line_reg_5 = QtGui.QLineEdit(self.centralwidget)
+        self.line_reg_5.setObjectName(_fromUtf8("line_reg_5"))
+        self.gridLayout.addWidget(self.line_reg_5, 5, 1, 1, 1)
+        self.but_exit = QtGui.QPushButton(self.centralwidget)
+        self.but_exit.setObjectName(_fromUtf8("but_exit"))
+        self.gridLayout.addWidget(self.but_exit, 6, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 333, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.but_exit, QtCore.SIGNAL(_fromUtf8("clicked()")), MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Firmware Control GUI", None))
+        self.but_read_reg_0.setText(_translate("MainWindow", "Read Reg0", None))
+        self.but_read_reg_1.setText(_translate("MainWindow", "Read Reg1", None))
+        self.but_read_reg_2.setText(_translate("MainWindow", "Read Reg2", None))
+        self.but_read_reg_3.setText(_translate("MainWindow", "Read Reg3", None))
+        self.but_read_reg_4.setText(_translate("MainWindow", "Read Reg4", None))
+        self.but_read_reg_5.setText(_translate("MainWindow", "Read Reg5", None))
+        self.but_exit.setText(_translate("MainWindow", "Exit", None))
+
--- dynamic_classes_python/main.py
+++ dynamic_classes_python/main.py
@@ -3,27 +3,34 @@ from PyQt4 import QtGui
 import imp
 import sys
 import signal
+import gui
 '''ctrl+c grabber to instantly kill the program'''
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 import fw_template
 fw_module = fw_template
 
-class Example(QtGui.QWidget):
+class MAINGUI(QtGui.QMainWindow):
     def __init__(self):
-        super(Example, self).__init__()
-        self.initUI()
-        self.fw_class = ''
-
-    def initUI(self):
-        dlg = QtGui.QFileDialog()
-        dlg.setFileMode(QtGui.QFileDialog.AnyFile)
-        dlg.setFilter("Text files (*.py)")
-        if dlg.exec_():
-            filename = dlg.selectedFiles()
-        self.fw_class = filename
-
+        QtGui.QMainWindow.__init__(self)
+        self.ui = gui.Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.ui.actionQuit.triggered.connect(QtGui.qApp.quit)
+        
+    def connect_buttons():
+        
+    def read_reg_0():
+        
 
 def main():
+    app = QApplication(sys.argv)
+    app.setStyleSheet(styleSheet)
+    window = MAINGUI()
+    window.show()
+    sys.exit(app.exec_())
+    
+    
+    '''
     '''Make a Qt app to start the GUI'''
     app = QtGui.QApplication(sys.argv)
     '''Trying to make it close gracefully'''
@@ -45,7 +52,32 @@ def main():
     fw_class.write_value('reg10', 0xDEADBEEF)
     fw_class.read_value('reg10')
     sys.exit(app.exec_())
-
+'''
 
 if __name__ == '__main__':
     main()
+    '''
+    class R2Gui(QMainWindow):    
+     def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.ui.actionQuit.triggered.connect(QtGui.qApp.quit)
+
+        self.ui.cb_lo_port.insertItem(0, 'phasematrix1')
+        self.ui.cb_lo_port.insertItem(1, 'phasematrix2')
+        self.ui.cb_lo_port.setCurrentIndex(0)
+        
+        self.ui.pb_reload_all.clicked.connect(self.reload_tones)
+        
+        
+        
+    if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet(styleSheet)
+    window = R2Gui()
+    window.show()
+    sys.exit(app.exec_())
+    '''
