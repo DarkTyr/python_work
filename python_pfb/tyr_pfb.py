c0b18261fa21e82ae4fbd004fe721147c1034a3f
DarkTyr <johnathon.gard@gmail.com>
2018-04-23 00:27:04 -0600

    /I now have it to were it will take the FFT of a long time stream in multiple sections and form a 2D array for X.

--- python_pfb/tyr_pfb.py
+++ python_pfb/tyr_pfb.py
@@ -4,11 +4,12 @@ import numpy as np
 pl.ion()
 
 #Signal to do math on
-f_res_mult = 6.25
-L = 256
+f_res_mult = 2.005
+nFrames = 256
+L = 256*nFrames
 t = np.linspace(0, L-1, L)
-x = (np.cos(2*np.pi*t*f_res_mult/L)+ 1j*np.sin(2*np.pi*t*f_res_mult/L))
-
+x = (np.cos(2*np.pi*t*f_res_mult/L*nFrames)+ 1j*np.sin(2*np.pi*t*f_res_mult/L*nFrames))
+plot_bin = 2
 
 #Flags
 plot_response = True
@@ -16,23 +17,26 @@ plot_log_power_response = True
 
 #Number of Taps 
 N = 256
-
+#number of steps
+steps = L/N
 #ADC sampling frequency in Hz
 fs = 200e6
 
 #Output array
-X = np.zeros((N), dtype=complex)
+X = np.zeros((N,steps), dtype=complex)
 # variable to store constants
 
 
 #Run the DFT algorithum
-for n in xrange(N):
-	for m in xrange(N):
-		X[n] += (x[m] * (np.cos(2*np.pi*n*m/N) - 1j*np.sin(2*np.pi*n*m/N)))
+for k in xrange(steps):
+	for n in xrange(N):
+		for m in xrange(N):
+			X[n][k] += (x[m+N*k] * (np.cos(2*np.pi*n*m/N) - 1j*np.sin(2*np.pi*n*m/N)))
 
 #Scale by something		
-X = X/(L)
+X = X/(N)
 
+index=4
 if(plot_response):
 	fig0 = pl.figure(0)
 	ax0 = fig0.add_subplot(311)
@@ -41,18 +45,26 @@ if(plot_response):
 	ax0.set_title("Input Signal")
 	ax0.grid(True)
 	ax1 = fig0.add_subplot(312)
-	ax1.plot(np.real(X))
-	ax1.plot(np.imag(X))
+	ax1.plot(np.real(X[:,index]))
+	ax1.plot(np.imag(X[:,index]))
 	ax1.set_title("Output")
 	ax1.grid(True)
 	ax2 = fig0.add_subplot(313)
-	ax2.plot(np.abs(X))
+	ax2.plot(np.abs(X[:,index]))
 	ax2.set_title("Magnitude or power of the output")
 	ax2.grid(True)
 
 if(plot_log_power_response):
 	fig1 = pl.figure(1)
-	ax0 = fig1.add_subplot(111)
-	ax0.plot(20*np.log10(np.abs(X)))
-	ax0.set_title("Power of Input in log form")
+	ax3 = fig1.add_subplot(111)
+	ax3.plot(20*np.log10(np.abs(X[:,index])))
+	ax3.set_title("Power of Input in log form")
+
+
+if(plot_bin!=0):
+	fig2 = pl.figure(2)
+	ax4 = fig2.add_subplot(111)
+	ax4.plot(np.real(X[plot_bin,:]))
+	ax4.plot(np.imag(X[plot_bin,:]))
+	ax4.set_title("Time sequency of Bin : " + str(plot_bin))
 
